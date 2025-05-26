d3d597186f9a5c018c81d43bea4c4677888fda84
Julien JEHL <julien.jehl@roboticia.com>
2025-05-26 13:20:38 +0200

    adapt starting script

--- deploy/stop_remote_front.sh
+++ deploy/stop_remote_front.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # To start the docker compose
-docker compose -p protecia-server --env-file ../combined-front-$1.env -f docker-compose-front-$1.yaml down
+docker compose -p protecia-front --env-file ../version-front-$1.env -f docker-compose-front-$1.yaml down
